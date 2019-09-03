a737c24dad5ea81eb58150f5a533762120a68181
Aylan Mello <aylanismello@gmail.com>
2019-09-03 20:20:58 +0100

    change meta tag

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,28 +1,21 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import { StaticQuery, graphql } from 'gatsby';
 
-import '../assets/scss/main.scss'
+import '../assets/scss/main.scss';
 
 const Layout = ({ children, location }) => {
-
   let content;
 
   if (location && location.pathname === '/') {
-    content = (
-      <div>
-        {children}
-      </div>
-    )
+    content = <div>{children}</div>;
   } else {
     content = (
       <div id="wrapper" className="page">
-        <div>
-          {children}
-        </div>
+        <div>{children}</div>
       </div>
-    )
+    );
   }
 
   return (
@@ -41,8 +34,16 @@ const Layout = ({ children, location }) => {
           <Helmet
             title={data.site.siteMetadata.title}
             meta={[
-              { name: 'description', content: 'Sample' },
+              {
+                name: 'description',
+                content: 'The online home of Aylan Mello'
+              },
               { name: 'keywords', content: 'sample, something' },
+              {
+                name: 'og:image',
+                content:
+                  'https://res.cloudinary.com/burncartel/image/upload/q_70/v1561453037/aylan.io-bg-1.jpg'
+              }
             ]}
           >
             <html lang="en" />
@@ -51,11 +52,11 @@ const Layout = ({ children, location }) => {
         </>
       )}
     />
-  )
-}
+  );
+};
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+  children: PropTypes.node.isRequired
+};
 
-export default Layout
+export default Layout;
