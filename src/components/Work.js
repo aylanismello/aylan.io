48b79c600465ff02ab81e6894251d99a6a68dc8c
Aylan Mello <aylanismello@gmail.com>
2019-09-03 19:57:31 +0100

    new work mroe stuff

--- src/components/Work.js
+++ src/components/Work.js
@@ -7,13 +7,16 @@ const cloudinaryImg = (imageName) => (
   `https://res.cloudinary.com/burncartel/image/upload/c_fit,q_70,w_1500/v1561453037/aylan.io-${imageName}.jpg`
 );
 
-const WorkPiece = ({ title, description, img, href }) => (
+const WorkPiece = ({ title, description, img, href, more }) => (
   <div className="WorkPiece">
     <div className="WorkPiece-headers">
       <a href={href} target="_blank">
         <h2 className="major">{title}</h2>
       </a>
-      <h3>{description}</h3>
+      <h3>[{description}]</h3>
+      <div>
+        {more}
+      </div>
     </div>
     <span className="image main">
       <a href={href} target="_blank">
@@ -54,19 +57,30 @@ const Work = ({ article, articleTimeout, imgSrc, close }) => (
       description="Weekly music discovery web app"
       img={cloudinaryImg('bc')}
       href="http://www.burncartel.com"
-    />
-    <WorkPiece
-      title="Bossafy"
-      description="Tool for jazz music composition"
-      img={cloudinaryImg('bossafy')}
-      href="http://bossafy.herokuapp.com/"
+      more="The home for my music lifestyle brand Burn Cartel: it's the place to discover our latest episodes and learn about great new music from around the world."
     />
     <WorkPiece
       title="Min Bark"
       description="Gatsby Website for German based multimedia artist"
       img={cloudinaryImg('min-bark')}
       href="http://min-bark.com"
+      more="Website made for Min Bark - where the main objective was to move off of Wordpress to create a more perfomant, pleasant viewing experience."
     />
+    <WorkPiece
+      title="MashupBot"
+      description="Musical Tool for making Mashups in the Browser"
+      img={cloudinaryImg('mashupbot')}
+      href="http://mashupbot.netlify.com"
+      more="Testing the boundaries of web audio technology by making a playful music app for on the fly creation."
+    />
+    {/* <WorkPiece
+      title="Bossafy"
+      description="Tool for jazz music composition"
+      img={cloudinaryImg('bossafy')}
+      href="http://bossafy.herokuapp.com/"
+      more=""
+    /> */}
+
     {/* <h2 className="major">Burn Cartel</h2>
     <h3>Weekly music discovery web app</h3>
     <span className="image main">
